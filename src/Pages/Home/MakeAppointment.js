dd048fcacfff2d7347fe02c591a751ccf9010995
Misrat121 <you@example.com>
2022-05-12 20:06:05 +0600

    appointment section done

---
+++ src/Pages/Home/MakeAppointment.js
@@ -0,0 +1,33 @@
+import React from "react";
+import doctor from "../../assets/images/doctor.png";
+import appointment from "../../assets/images/appointment.png";
+import PrimaryButton from "../Shared/PrimaryButton";
+
+const MakeAppointment = () => {
+  return (
+    <section
+      style={{
+        background: `url(${appointment})`,
+      }}
+      className="flex justify-center items-center"
+    >
+      <div className="flex-1 hidden lg:block">
+        <img className="mt-[-100px]" src={doctor} alt="" />
+      </div>
+      <div className="flex-1">
+        <h3 className="text-xl text-primary font-bold">Appointment</h3>
+        <h2 className="text-3xl text-white">Make an Appointment Today</h2>
+        <p className="text-white py-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          voluptate sit consequuntur neque optio, odio consequatur, ipsum nulla
+          quisquam, laudantium minus! Id molestias provident sequi, mollitia
+          blanditiis porro ullam odit, quasi nihil debitis illum laborum.
+          Exercitationem nihil corrupti molestias fuga?
+        </p>
+        <PrimaryButton>Get Started</PrimaryButton>
+      </div>
+    </section>
+  );
+};
+
+export default MakeAppointment;
