866786d9d241ba906a1c5f335b2ce555249ee8e3
Misrat121 <you@example.com>
2022-05-13 11:31:44 +0600

    appointment banner added using react  day picker

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -14,10 +14,10 @@ const MakeAppointment = () => {
       <div className="flex-1 hidden lg:block">
         <img className="mt-[-100px]" src={doctor} alt="" />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 px-5 ">
         <h3 className="text-xl text-primary font-bold">Appointment</h3>
-        <h2 className="text-3xl text-white">Make an Appointment Today</h2>
-        <p className="text-white py-5">
+        <h2 className="text-3xl text-white py-5">Make an Appointment Today</h2>
+        <p className="text-white pb-5">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
           voluptate sit consequuntur neque optio, odio consequatur, ipsum nulla
           quisquam, laudantium minus! Id molestias provident sequi, mollitia
