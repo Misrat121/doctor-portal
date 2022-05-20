c5656ff7d2a08039f7df4ef4bb65eb1b35c31624
Misrat121 <you@example.com>
2022-05-20 15:12:12 +0600

    sign in with google done

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,9 +3,9 @@ import React from "react";
 const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
-    <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title text-secondary">{name}</h2>
+    <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title text-secondary">{name}</h2>
         <p>
           {slots.length > 0 ? (
             <span>{slots[0]}</span>
@@ -17,13 +17,13 @@ const Service = ({ service, setTreatment }) => {
           {slots.length} {slots.length > 1 ? "spaces" : "space"} spaces
           available
         </p>
-        <div class="card-actions justify-center">
-          <button class=""></button>
+        <div className="card-actions justify-center">
+          <button className=""></button>
           <label
             for="booking-modal"
             disabled={slots.length === 0}
             onClick={() => setTreatment(service)}
-            class="btn btn-primary text-center text-white uppercase"
+            className="btn btn-primary text-center text-white uppercase"
           >
             Book Appointment
           </label>
